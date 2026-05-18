a359bb95022fe33b61563271d473b1c38336def1
Parsa Faraji Alamouti <165321600+parsa-faraji@users.noreply.github.com>
2026-05-18 00:50:32 -0700

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if strs == []:
+            return ""
+        encoded = ""
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+        return encoded
+
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string back to a list of strings."""
+        result = []
+        i = 0
+        while i < len(s):
+            # find the delimiter '#'
+            j = i
+            while s[j] != '#':
+                j += 1
+            # length of the next string
+            length = int(s[i:j])
+            # move past the '#'
+            start = j + 1
+            # extract the string
+            result.append(s[start:start + length])
+            # move i to the start of the next length field
+            i = start + length
+        return result
